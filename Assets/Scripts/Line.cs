6f1e45f2256f4d05e2d8f1a5467edd55c5e5e2f6
Meavor <habinezj@gmail.com>
2018-08-19 15:09:04 -0400

    "State machine added"

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -2,24 +2,20 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Line : MonoBehaviour {
-
+public class Line : MonoBehaviour
+{
 	public LineRenderer lineRenderer;
 	public EdgeCollider2D edgeCol;
+    public LineType type;
+    public float length = 15f;
 
 	List<Vector2> points;
+    public Vector2 start { get { return points[0]; } }
+    public Vector2 end { get { return points[points.Count - 1]; } }
 
-	public void UpdateLine (Vector2 mousePos)
+	public void CreateLine ()
 	{
-		if (points == null)
-		{
-			points = new List<Vector2>();
-			SetPoint(mousePos);
-			return;
-		}
-
-		if (Vector2.Distance(points.Last(), mousePos) > .1f)
-			SetPoint(mousePos);
+		
 	}
 
 	void SetPoint (Vector2 point)
@@ -33,4 +29,11 @@ public class Line : MonoBehaviour {
 			edgeCol.points = points.ToArray();
 	}
 
+}
+
+public enum LineType
+{
+    Straight,
+    HillDown,
+    LineDown,
 }
